9c655409a5e3b32d50a5efc227a84deabdef32d5
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-11-24 09:19:54 +0530

    Assignment 1-Enpenses component added

---
+++ 01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,32 @@
+import ExpenseItems from "./ExpenseItems";
+import "./Expenses.css";
+
+function IndividualExpenses(props) {
+  const expenses = props.expenses;
+  return (
+    <div className="expenses">
+      <ExpenseItems
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItems>
+    </div>
+  );
+}
+
+export default IndividualExpenses;
