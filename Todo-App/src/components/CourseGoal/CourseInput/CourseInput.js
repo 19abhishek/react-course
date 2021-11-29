5f3ec0eaba604f4b3322f3d628d5253a0e3ec15f
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-11-29 16:34:32 +0530

    Course Goal updated

--- Todo-App/src/components/CourseGoal/CourseInput/CourseInput.js
+++ Todo-App/src/components/CourseGoal/CourseInput/CourseInput.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./CourseInput.css";
 import Button from "../../UI/Button.js";
 
 const CourseInput = (props) => {
+  const [enteredValue, setInputState] = useState("");
+
+  const onChangeEventHandler = (e) => {
+    setInputState(e.target.value);
+  };
+
   const formSubmitHandler = (e) => {
     e.preventDefault();
+    props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form_input">
-        <div className="text">Course Goal</div>
-        <input type="text" />
+      <div className="form-control">
+        <label>Course Goal</label>
+        <input type="text" onChange={onChangeEventHandler} />
       </div>
-      <Button type="submit" />
+      <Button type="submit">Add Goal</Button>
     </form>
   );
 };
