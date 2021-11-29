442952dc92301519debb22b0515cbe1bc51a5935
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-11-29 10:53:59 +0530

    Todo app start

---
+++ Todo-App/src/components/CourseGoal/CourseInput/CourseInput.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CourseInput = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
+  );
+};
+
+export default CourseInput;
---
+++ Todo-App/src/components/UI/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Button.css";
+
+function Button() {
+  return <button type="submit">Add Item</button>;
+}
+
+export default Button;
