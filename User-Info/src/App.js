6d77abca3c1dc8adb5e3879eab90cc0808a7a297
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-01 07:58:01 +0530

    User Input and Info UI done

--- User-Info/src/App.js
+++ User-Info/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import UserInput from "./Components/User/UserInput";
 import UserItem from "./Components/User/UserItem";
+import UserList from "./Components/User/UserList";
 import Card from "./Components/Card/Card";
 import "./App.css";
 
@@ -9,26 +10,29 @@ function App() {
   const [users, setUser] = useState([
     { name: "Abhishek", age: 23, id: Math.random().toString() },
     { name: "Sneha", age: 25, id: Math.random().toString() },
+    { name: "Shubham", age: 25, id: Math.random().toString() },
   ]);
 
-  let content = (
-    <Card>
-      <p>No User data available.</p>
-    </Card>
-  );
+  // let content = (
+  //   <Card>
+  //     <p>No User data available.</p>
+  //   </Card>
+  // );
 
-  if (users.length > 0) {
-    content = users.map((user) => <UserItem name={user.name} age={user.age} />);
-  }
+  // if (users.length > 0) {
+  //   content = users.map((user) => <UserItem name={user.name} age={user.age} />);
+  // }
 
   return (
     <div className="main">
-      <section className="input">
+      <div className="input">
         <Card>
           <UserInput />
         </Card>
-      </section>
-      <section className="output">{content}</section>
+      </div>
+      <div className="output">
+        <UserList items={users} />
+      </div>
     </div>
   );
 }
--- User-Info/src/Components/User/UserList.js
+++ User-Info/src/Components/User/UserList.js
@@ -1,5 +1,26 @@
 import React from "react";
 
-const UserList = (props) => {};
+import UserItem from "./UserItem";
+import Card from "../Card/Card";
+import "./UserList.css";
+
+const UserList = (props) => {
+  const users = props.items;
+  console.log(users);
+
+  let content = <p>No User data available.</p>;
+
+  if (users.length > 0) {
+    content = users.map((user) => (
+      <UserItem name={user.name} age={user.age} key={user.id} />
+    ));
+  }
+
+  return (
+    <div className="user-list">
+      <Card>{content}</Card>
+    </div>
+  );
+};
 
 export default UserList;
