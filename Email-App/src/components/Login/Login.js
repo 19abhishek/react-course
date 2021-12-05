ae6b82a656032827d650c280fc8b0440c531d0cf
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-05 12:11:29 +0530

    Building and Using a custom context provider component

--- Email-App/src/components/Login/Login.js
+++ Email-App/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state, action) => {
   if (action.type === "USER_INFO") {
@@ -40,6 +41,8 @@ const Login = (props) => {
     value: "",
     isValid: false,
   });
+
+  const authCtx = useContext(AuthContext);
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -88,7 +91,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value.trim());
+    authCtx.onLogin(emailState.value, passwordState.value.trim());
   };
 
   return (
