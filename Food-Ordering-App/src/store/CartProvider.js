5ac1d85d9890689a0cae3d38a57b08aa8a29b0af
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-09 11:16:45 +0530

    Modified CartProviderReducer to add items in cart

--- Food-Ordering-App/src/store/CartProvider.js
+++ Food-Ordering-App/src/store/CartProvider.js
@@ -9,11 +9,28 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updateItems = state.items.concat(action.item);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     const updatedItemPrice =
       state.totalAmount + action.item.price * action.item.amount;
     return {
-      items: updateItems,
+      items: updatedItems,
       totalAmount: updatedItemPrice,
     };
   }
