589e2b9a4ee0a9a63515d556930c8f51cce970b8
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-10 17:19:37 +0530

    useMemo used for optimising arrays

--- Behind-The-Scenes-React/src/App.js
+++ Behind-The-Scenes-React/src/App.js
@@ -1,30 +1,40 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 import "./App.css";
-import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
 import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [paragraphState, setParagraphState] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
-  console.log("App running");
+  // const [paragraphState, setParagraphState] = useState(false);
+  // const [allowToggle, setAllowToggle] = useState(false);
+  // console.log("App running");
 
-  const clickHandler = useCallback(() => {
-    if (allowToggle) {
-      setParagraphState((currState) => !currState);
-    }
-  }, [allowToggle]);
+  // const clickHandler = useCallback(() => {
+  //   if (allowToggle) {
+  //     setParagraphState((currState) => !currState);
+  //   }
+  // }, [allowToggle]);
 
-  const allowToggleHandler = () => {
-    setAllowToggle((currToggle) => !currToggle);
-  };
+  // const allowToggleHandler = () => {
+  //   setAllowToggle((currToggle) => !currToggle);
+  // };
+
+  const [listTitle, setListTitle] = useState("My List");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={paragraphState} />
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+      {/* <DemoOutput show={paragraphState} />
       <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
-      <Button onClick={clickHandler}>Toggle Paragraph!</Button>
+      <Button onClick={clickHandler}>Toggle Paragraph!</Button> */}
     </div>
   );
 }
