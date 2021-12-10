09bee7df62c31d4977e6550f67b8456c9f01a849
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-10 15:48:26 +0530

    Preventing unnecessary re-evaluation using React.memo()

--- Behind-The-Scenes-React/src/App.js
+++ Behind-The-Scenes-React/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [paragraphState, setParagraphState] = useState(false);
+  console.log("Paragraph running");
+
+  const clickHandler = () => {
+    setParagraphState((currState) => (currState = !currState));
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={paragraphState} />
+      <Button onClick={clickHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
