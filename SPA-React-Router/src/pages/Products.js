1e67a9f43b64afbbdb486976f12e4f5f1a5eb9c7
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-27 13:28:29 +0530

    Using Switch and exact for configuring exact routes

--- SPA-React-Router/src/pages/Products.js
+++ SPA-React-Router/src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Product = () => {
   return (
     <section>
       <h1>Here is the product list.</h1>
       <ul>
-        <li>A Book</li>
-        <li>A carpet</li>
-        <li>An online course</li>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">An online course</Link>
+        </li>
       </ul>
     </section>
   );
