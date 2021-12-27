015a3bbab6fa2995b58fce037d521ca872f1c285
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-27 13:17:16 +0530

    Adding dynamic routes with params

--- SPA-React-Router/src/pages/Products.js
+++ SPA-React-Router/src/pages/Products.js
@@ -1,5 +1,14 @@
 const Product = () => {
-  return <h1>Here is the product list.</h1>;
+  return (
+    <section>
+      <h1>Here is the product list.</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A carpet</li>
+        <li>An online course</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Product;
