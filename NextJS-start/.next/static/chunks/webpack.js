d668d986b13d02675973ace840e7bee21881ae22
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-30 17:23:32 +0530

    Linking between pages

--- NextJS-start/.next/static/chunks/webpack.js
+++ NextJS-start/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f951ac2c8ac61856de2b";
+/******/ 	var hotCurrentHash = "a5a4994553212f8efc68";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- NextJS-start/pages/news/index.js
+++ NextJS-start/pages/news/index.js
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>Welcome to the news Page!!</h1>;
+  return (
+    <Fragment>
+      <h1>Welcome to the news Page!!</h1>
+      <ul>
+        <li>
+          <Link href="/news/about">About</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
