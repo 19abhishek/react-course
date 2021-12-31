cd3da5c8c21d4504c1747d47dae1cd8b3ca759de
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-31 16:51:16 +0530

    revalidate added to keeo data up-to-date

--- NextJs-Project/.next/static/chunks/webpack.js
+++ NextJs-Project/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "45c47080d293e8415792";
+/******/ 	var hotCurrentHash = "8e6c50ace78dc9dc18f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
