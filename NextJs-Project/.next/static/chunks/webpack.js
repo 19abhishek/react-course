7bf1f88679a0c665ef1e851a1f660c3d22523639
Abhishek Kumar <abhishek.kum1908@gmail.com>
2021-12-31 16:03:26 +0530

    Data fetching for Static Pages

--- NextJs-Project/.next/static/chunks/webpack.js
+++ NextJs-Project/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f173b68ae14601e0a089";
+/******/ 	var hotCurrentHash = "45c47080d293e8415792";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- NextJs-Project/pages/index.js
+++ NextJs-Project/pages/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -21,24 +20,18 @@ const DUMMY_MEETUPS = [
 ];
 
 const HomePage = (props) => {
-  const [loadedMeetups, setLoadedMeetups] = useState([]);
-
-  useEffect(() => {
-    setLoadedMeetups(DUMMY_MEETUPS);
-  }, []);
-
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  return <MeetupList meetups={props.meetups} />;
 };
 
 //For loading data before the functions loads
 
-// export async function getStaticProps() {
-//   //fetch data from an API
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//   };
-// }
+export async function getStaticProps() {
+  //fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
 
 export default HomePage;
