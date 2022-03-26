c77495dcb6fffaa027cded6ac47811f27ddd8c15
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-03-26 23:14:15 +0530

    TypeScript basics

---
+++ Typescript/with-typescript.js
@@ -0,0 +1,5 @@
+// function addTS(a, b) {
+//     return a + b;
+// }
+// var res = addTS("2", 5);
+// console.log(res);
---
+++ Typescript/with-typescript.ts
@@ -0,0 +1,7 @@
+function addTS(a: number, b: number) {
+  return a + b;
+}
+
+const res = addTS(2, 5);
+
+console.log(res);
